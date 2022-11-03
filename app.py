d9617f38bf09bf874f07b3c7ffacff6dcd6b3252
Joshua Hooper <joshuahooper3@gmail.com>
2022-11-02 19:35:27 -0500

    added inputs html/css

--- app.py
+++ app.py
@@ -1,13 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sections.db'
+db = SQLAlchemy(app)
+
+
+class Sections(db.Model):
+    crn = db.Column(db.Integer, primary_key=True)
+    subject = db.Column(db.String(200), nullable=True)
+    course = db.Column(db.String(200), nullable=True)
+    section = db.Column(db.String(200), nullable=True)
+    section_title = db.Column(db.String(200), nullable=True)
+    primary_instructor = db.Column(db.String(200), nullable=True)
+    course_dates = db.Column(db.String(200), nullable=True)
+    seats_left = db.Column(db.String(200), nullable=True)
+    wait_list = db.Column(db.String(200), nullable=True)
 
 
 @app.route('/')
 def home():
-    return "Flask heroku app."
+    return "<h1>Home Page</h1>"
+
+
+@app.route('/inputs', methods=['GET', 'POST'])
+def inputs():
+    return render_template('edit.html')
 
 
 if __name__ == '__main__':
     app.run()
-
